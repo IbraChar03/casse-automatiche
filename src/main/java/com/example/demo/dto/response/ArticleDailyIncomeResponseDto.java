b9fadf2bf8a07afb419036006dc8fa6b7c03a1bd
Ibrahim Char <ibrachar68@gmail.com>
2025-01-30 16:01:27 +0100

    api calcolo articoli venduti

--- src/main/java/com/example/demo/dto/response/ArticleDailyIncomeResponseDto.java
+++ src/main/java/com/example/demo/dto/response/ArticleDailyIncomeResponseDto.java
@@ -1,14 +1,16 @@
 package com.example.demo.dto.response;
 
 import com.example.demo.entity.Article;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class ArticleDailyIncomeResponseDto {
     private Article article;
     private Long articleQuantitySold;
-    private Long dailyIncome;
+    private Double dailyIncome;
 
 }
--- src/main/java/com/example/demo/service/ArticleService.java
+++ src/main/java/com/example/demo/service/ArticleService.java
@@ -2,16 +2,19 @@ package com.example.demo.service;
 
 import com.example.demo.dto.request.ArticlesDailyIncomeRequestDto;
 import com.example.demo.dto.request.StockDateRequestDto;
+import com.example.demo.dto.response.ArticleDailyIncomeResponseDto;
 import com.example.demo.dto.response.ArticleListDailyIncomeResponseDto;
 import com.example.demo.entity.Article;
 import com.example.demo.entity.Receipt;
 import com.example.demo.entity.Stock;
 import com.example.demo.repository.ArticleRepository;
+import com.example.demo.repository.PriceRepository;
 import com.example.demo.repository.ReceiptRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -21,13 +24,36 @@ public class ArticleService {
     private ArticleRepository articleRepository;
     @Autowired
     private ReceiptRepository receiptRepository;
+    @Autowired
+    private PriceRepository priceRepository;
 
 
     public Article addArticle(Article article) {
         return articleRepository.save(article);
     }
     public ArticleListDailyIncomeResponseDto calculateArticlesDailyIncome(ArticlesDailyIncomeRequestDto articlesDailyIncomeRequestDto) {
-        return null;
+        ArticleListDailyIncomeResponseDto articleListDailyIncomeResponseDto = new ArticleListDailyIncomeResponseDto();
+        articleListDailyIncomeResponseDto.setArticles(new ArrayList<>());
+        LocalDate date = LocalDate.parse(articlesDailyIncomeRequestDto.getDate());
+        List<Receipt> receipts = receiptRepository.findAllByEmissionDate(date);
+
+        List<Long> allArticleIds = receipts.stream()
+                .flatMap(receipt -> receipt.getArticleIds().stream())
+                .toList();
+
+        allArticleIds.stream()
+                .distinct()
+                .forEach(articleId -> {
+                    Long articleSold = allArticleIds.stream()
+                            .filter(id -> id.equals(articleId))
+                            .count();
+                    var price = priceRepository.findTopByArticleIdAndValidityDateLessThanEqualOrderByValidityDateDesc(
+                            articleId, date).orElseThrow();
+                    Double dailyIncome =price.getValue() * articleSold;
+                    var article = articleRepository.findById(articleId).orElseThrow();
+                    articleListDailyIncomeResponseDto.getArticles().add(new ArticleDailyIncomeResponseDto(article,articleSold,dailyIncome));
+                });
+        return articleListDailyIncomeResponseDto;
     }
 
 
